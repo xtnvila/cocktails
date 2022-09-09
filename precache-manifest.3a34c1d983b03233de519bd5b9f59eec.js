af50b0ff50c0c649392a1e6c81cc739ae77e2836
Cris Vila <gz.vila@gmail.com>
2022-09-09 18:54:46 +0200

    Updates

---
+++ precache-manifest.3a34c1d983b03233de519bd5b9f59eec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbcc781c85c347c5ccc1d92e2e0420a8",
+    "url": "/cocktails/index.html"
+  },
+  {
+    "revision": "ceaf159cfff74e01ea2d",
+    "url": "/cocktails/static/css/main.805a2641.chunk.css"
+  },
+  {
+    "revision": "e1f6309a6f0d3e350015",
+    "url": "/cocktails/static/js/2.72303b8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cocktails/static/js/2.72303b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceaf159cfff74e01ea2d",
+    "url": "/cocktails/static/js/main.a3f7f6eb.chunk.js"
+  },
+  {
+    "revision": "e448c4ac95b4b7fbb491",
+    "url": "/cocktails/static/js/runtime-main.42b4d6f2.js"
+  },
+  {
+    "revision": "f3ad766494d1f9701717c1a860cd5b10",
+    "url": "/cocktails/static/media/logo.f3ad7664.svg"
+  }
+]);
